dca273ae0b68f2ddbe183a93ca2e45347a80fc3b
Alek2ander <Alek2ander@users.noreply.github.com>
2023-12-22 15:47:07 +0400

    Day 21 improve solution

--- 2023/21.py
+++ 2023/21.py
@@ -6,21 +6,14 @@ with (open('21.txt', 'r') as in_file):
     starting_point = None
     grid = [line for line in in_file.read().split('\n')]
     max_y, max_x = len(grid), len(grid[0])
-    even_full = odd_full = 0
     for y, line in enumerate(grid):
-        for x, c in enumerate(line):
-            if c != '#':
-                if (x + y) % 2 == 0:
-                    even_full += 1
-                else:
-                    odd_full += 1
         if (x := line.find('S')) != -1:
             starting_point = (x, y)
     max_y, max_x = len(grid), len(grid[0])
 queue = deque([starting_point])
 visited = {starting_point: 0}
-goals = [goal_1, max_x // 2, max_x // 2 + max_x]
-counts = [0, 0, 0]
+goals = [goal_1, max_x // 2 + max_x * 2]
+zone_counts = [{(x, y): 0 for x in range(-3, 4) for y in range(-3, 4)} for _ in range(len(goals))]
 while len(queue):
     x, y = queue.popleft()
     steps = visited[x, y]
@@ -28,16 +21,28 @@ while len(queue):
         continue
     for i, goal in enumerate(goals):
         if steps <= goal and (goal - steps) % 2 == 0:
-            counts[i] += 1
+            zone_counts[i][(x // max_x, y // max_y)] += 1
     for nx, ny in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
         if grid[ny % max_y][nx % max_x] == '#' or (nx, ny) in visited:
             continue
         visited[nx, ny] = steps + 1
         queue.append((nx, ny))
 
-result_1, odd_inner, fx1 = tuple(counts)
-print(result_1)
-even_outer = fx1 - 2 * odd_full - 2 * odd_inner - even_full
+print(zone_counts[0][0, 0])
+
+#  0      n     1     n     0
+#     /     .       .    \
+#  n     n-1   n^2   n-1    n
+#       .    /     \    .
+#  1     n^2 (n-1)^2 n^2    1
+#       .    \     /    .
+#  n     n-1   n^2   n-1    n
+#      \    .       .    /
+#  0      n     1     n     0
 x = (goal_2 - (max_x // 2)) // max_x
-result_2 = (x + 1) * x * odd_full + (x + 1) * odd_inner + x * x * even_full + x * even_outer - x * (x - 1)
-print(result_2)
+parts = zone_counts[1]
+print(parts[0, 0] * (x - 1) * (x - 1) + parts[0, 1] * x * x
+      + (parts[1, 1] + parts[1, -1] + parts[-1, 1] + parts[-1, -1]) * (x - 1)
+      + (parts[2, 0] + parts[-2, 0] + parts[0, 2] + parts[0, -2])
+      + (parts[2, 1] + parts[2, -1] + parts[-2, 1] + parts[-2, -1]) * x
+      )
